2dc70c57c17d52f6e4d21ffc0b2bf0e3f1cf357e
Mauricio Falbo <mauriciofalbo.rj@gmail.com>
2020-03-10 18:00:46 -0300

    Mail e Cancelled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '0318ca909a8364',
+    pass: 'da5d627887ed8b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
