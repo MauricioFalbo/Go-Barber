ab7ca342653d149e0c038f077e77ae61727f4a53
Mauricio Falbo <mauriciofalbo.rj@gmail.com>
2020-03-11 15:29:42 -0300

    Variaveis

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '1a6cdeeeb5bb93413d94b6ef5428b76e',
-  expiresIn: '7d',
+  secret: process.env.APP_SECRET,
+  expiresIn: process.env.APP_EXPIRESIN,
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0318ca909a8364',
-    pass: 'da5d627887ed8b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
